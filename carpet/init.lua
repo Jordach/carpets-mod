2dfcf74e6619c7530cebeec5915052396d33f7cd
Jordan Snelling <jordach.snelling@gmail.com>
2012-08-26 15:54:21 +0100

    add textures + final node defs;
    last thing to add is crafting

--- carpet/init.lua
+++ carpet/init.lua
@@ -42,7 +42,7 @@ minetest.register_node("carpet:orange", {
 minetest.register_node("carpet:yellow", {
 	description = "Yellow Carpet",
 	drawtype = "raillike",
-	tiles = {"carpet_yellow_out.png", "carpet_yellow_cor.png", "carpet_yellow_one.png", "carpet_yellow_cen.png"},
+	tiles = {"carpet_yellow_out.png"},
 	inventory_image = "carpet_yellow_out.png",
 	wield_image = "carpet_yellow_out.png",
 	paramtype = "light",
@@ -60,7 +60,7 @@ minetest.register_node("carpet:yellow", {
 minetest.register_node("carpet:green", {
 	description = "Green Carpet",
 	drawtype = "raillike",
-	tiles = {"carpet_green_out.png", "carpet_green_cor.png", "carpet_green_one.png", "carpet_green_cen.png"},
+	tiles = {"carpet_green_out.png"},
 	inventory_image = "carpet_green_out.png",
 	wield_image = "carpet_green_out.png",
 	paramtype = "light",
@@ -78,7 +78,7 @@ minetest.register_node("carpet:green", {
 minetest.register_node("carpet:cyan", {
 	description = "Cyan Carpet",
 	drawtype = "raillike",
-	tiles = {"carpet_cyan_out.png", "carpet_cyan_cor.png", "carpet_cyan_one.png", "carpet_cyan_cen.png"},
+	tiles = {"carpet_cyan_out.png"},
 	inventory_image = "carpet_cyan_out.png",
 	wield_image = "carpet_cyan_out.png",
 	paramtype = "light",
@@ -96,7 +96,7 @@ minetest.register_node("carpet:cyan", {
 minetest.register_node("carpet:blue", {
 	description = "Blue Carpet",
 	drawtype = "raillike",
-	tiles = {"carpet_blue_out.png", "carpet_blue_cor.png", "carpet_blue_one.png", "carpet_blue_cen.png"},
+	tiles = {"carpet_blue_out.png"},
 	inventory_image = "carpet_blue_out.png",
 	wield_image = "carpet_blue_out.png",
 	paramtype = "light",
@@ -114,7 +114,7 @@ minetest.register_node("carpet:blue", {
 minetest.register_node("carpet:pink", {
 	description = "Pink Carpet",
 	drawtype = "raillike",
-	tiles = {"carpet_pink_out.png", "carpet_pink_cor.png", "carpet_pink_one.png", "carpet_pink_cen.png"},
+	tiles = {"carpet_pink_out.png"},
 	inventory_image = "carpet_pink_out.png",
 	wield_image = "carpet_pink_out.png",
 	paramtype = "light",
@@ -129,28 +129,11 @@ minetest.register_node("carpet:pink", {
 	groups = {dig_immediate=2},
 })
 
-minetest.register_node("carpet:white", {
-	description = "White Carpet",
-	drawtype = "raillike",
-	tiles = {"carpet_white_out.png", "carpet_white_cor.png", "carpet_white_one.png", "carpet_white_cen.png"},
-	inventory_image = "carpet_white_out.png",
-	wield_image = "carpet_white_out.png",
-	paramtype = "light",
-	is_ground_content = true,
-	walkable = false,
-	buildable_to = true,
-	selection_box = {
-		type = "fixed",
-                
-                fixed = {-1/2, -1/2, -1/2, 1/2, -1/2+1/16, 1/2},
-	},
-	groups = {dig_immediate=2},
-})
 
 minetest.register_node("carpet:black", {
 	description = "Black Carpet",
 	drawtype = "raillike",
-	tiles = {"carpet_black_out.png", "carpet_black_cor.png", "carpet_black_one.png", "carpet_black_cen.png"},
+	tiles = {"carpet_black_out.png"},
 	inventory_image = "carpet_black_out.png",
 	wield_image = "carpet_black_out.png",
 	paramtype = "light",
@@ -168,7 +151,7 @@ minetest.register_node("carpet:black", {
 minetest.register_node("carpet:magenta", {
 	description = "Magenta Carpet",
 	drawtype = "raillike",
-	tiles = {"carpet_magenta_out.png", "carpet_magenta_cor.png", "carpet_magenta_one.png", "carpet_magenta_cen.png"},
+	tiles = {"carpet_magenta_out.png"},
 	inventory_image = "carpet_magenta_out.png",
 	wield_image = "carpet_magenta_out.png",
 	paramtype = "light",
@@ -183,4 +166,24 @@ minetest.register_node("carpet:magenta", {
 	groups = {dig_immediate=2},
 })
 
+minetest.register_node("carpet:white", {
+	description = "White Carpet",
+	drawtype = "raillike",
+	tiles = {"carpet_white_out.png"},
+	inventory_image = "carpet_white_out.png",
+	wield_image = "carpet_white_out.png",
+	paramtype = "light",
+	is_ground_content = true,
+	walkable = false,
+	buildable_to = true,
+	selection_box = {
+		type = "fixed",
+                
+                fixed = {-1/2, -1/2, -1/2, 1/2, -1/2+1/16, 1/2},
+	},
+	groups = {dig_immediate=2},
+})
+
 -- Crafing
+
+-- TODO
